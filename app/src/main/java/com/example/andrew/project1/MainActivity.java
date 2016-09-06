64805ca5fe171ec384e1f53a8c9215222e6bd824
Andrew Volkmann <asvolk13@gmail.com>
2016-09-06 16:21:49 -0400

    last fixes

--- app/src/main/java/com/example/andrew/project1/MainActivity.java
+++ app/src/main/java/com/example/andrew/project1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.andrew.project1;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText firstnumber = null;
+    private EditText firstBase = null;
+    private EditText newBase = null;
+    private Button convertButton =null;
+    private TextView finaltext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
         //get the text fields
 
-        final EditText firstnumber = (EditText) findViewById(R.id.firstNumber);
-        final EditText firstBase = (EditText) findViewById(R.id.Base);
-        final EditText newBase = (EditText) findViewById(R.id.newBase);
-        final Button convertButton = (Button) findViewById(R.id.ConvertButton);
-        final TextView finaltext = (TextView) findViewById(R.id.finalNumber);
+        this.firstnumber = (EditText) findViewById(R.id.firstNumber);
+        this.firstBase = (EditText) findViewById(R.id.Base);
+        this.newBase = (EditText) findViewById(R.id.newBase);
+        this.convertButton = (Button) findViewById(R.id.ConvertButton);
+        this.finaltext = (TextView) findViewById(R.id.finalNumber);
 
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                firstBase.setBackgroundColor(getResources().getColor(R.color.blank));
+                firstBase.setBackgroundColor(Color.TRANSPARENT);
+
                 int originalB = Integer.parseInt(firstBase.getText().toString());
                 int base = Integer.parseInt(newBase.getText().toString());
                 String newNumber = "";
@@ -44,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
                 } catch (NumberFormatException e) {
                     Toast.makeText(MainActivity.this, R.string.Toast, Toast.LENGTH_LONG).show();
                     firstBase.setBackgroundColor(getResources().getColor(R.color.errorColor));
+
+
                 }
                 finaltext.setText(newNumber);
             }
